9f92afabbbe7a36649fc63fcf88a079c87046b6d
Phine1 <83701551+Phine1@users.noreply.github.com>
2022-03-27 19:18:52 +0000

    couldnt add tailwind components

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
 module.exports = {
   content: ["./resources/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+    colors: {
+      'primary': '#F0E5CF',
+      'second': '#f7f6f2',
+      'third': 'c8c6c6',
+      'fourth': '#4b6587'
+    },
+    'fontFamily': {
+      'amita': ['Amita', 'cursive', 'sans-serif', ...defaultTheme.fontFamily.sans]
+    }
+    },
   },
   plugins: [],
 }
