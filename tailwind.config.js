27d52d8aaaaccfeafc0e679f76a03c11cf27c008
Phine1 <83701551+Phine1@users.noreply.github.com>
2022-03-28 08:29:02 +0000

    reset-up project, added tailwindcss again

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,15 +2,12 @@ module.exports = {
   content: ["./resources/**/*.{html,js}"],
   theme: {
     extend: {
-    colors: {
-      'primary': '#F0E5CF',
-      'second': '#f7f6f2',
-      'third': 'c8c6c6',
-      'fourth': '#4b6587'
-    },
-    'fontFamily': {
-      'amita': ['Amita', 'cursive', 'sans-serif', ...defaultTheme.fontFamily.sans]
-    }
+      colors: {
+        'primary': '#F0E5CF',
+        'second': '#f7f6f2',
+        'third': 'c8c6c6',
+        'fourth': '#4b6587'
+      }
     },
   },
   plugins: [],
